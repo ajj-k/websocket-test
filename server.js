c83c084c84ff8ae0922ac4e774f04e10d7600bbf
tororo <bouando4ha@gmail.com>
2022-03-24 21:47:15 +0900

    パラメータが取得できない

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.unsubscribe(express.urlencoded({extended: true}));
+
 //ルーティング
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
     console.log("/ check");
+});
+
+app.get("/check", (req, res) => {
+    res.sendFile(`${__dirname}/public/check.html`);
+    console.log("/check check");
+});
+
+app.post("/check", (req, res) => {
+    const name = req.body.name;
+    const context = req.body.context;
+    res.send(`name: ${name}, context: ${context}`);
 });
 
 //HTTPサーバ起動
